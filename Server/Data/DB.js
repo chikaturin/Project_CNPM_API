9ef551df8645c628a2c559d83ccb3f1543729fc5
Chikaturin <ninhhoaithanh@gmail.com>
2024-07-21 14:21:17 +0700

    deploy node

--- Server/Data/DB.js
+++ Server/Data/DB.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
+
 mongoose
-  .connect(
-    "mongodb+srv://chikaturin:123@cluster0.zi5ms1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  )
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connected to MongoDB!");
   })
   .catch((e) => {
     console.error("Did not connect to MongoDB", e);
   });
+
 module.exports = mongoose;
